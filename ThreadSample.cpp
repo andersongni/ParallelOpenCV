354b0dbfca64b14784c1eab0770fd8a46717aede
Anderson Ibrahim <andersongni.3@gmail.com>
2018-07-29 00:13:40 -0300

    Casos de uso com thread serializer

--- ThreadSample.cpp
+++ ThreadSample.cpp
@@ -1,3 +1,9 @@
+/*
+ * Anderson Ibrahim
+ * 
+ * g++ -pthread ThreadSample.cpp -o ThreadSample.cpp.out -std=c++0x && ./ThreadSample.cpp.out
+ * 
+ */ 
 #include <iostream>
 #include <thread>
 #include <vector>
@@ -24,7 +30,7 @@ bool frameCompare(const taggedFrame& first, const taggedFrame& second) {
 
 void serialize () {
 	
-	int next = 1;
+	int next = 0;
 
 	while (true) {
 		std::unique_lock<mutex> locker(mu);
@@ -33,9 +39,10 @@ void serialize () {
 		sort(buffer.begin(), buffer.end(), frameCompare);
 		
 		while (next == buffer.front().tag) {
-			cout << "Writing frame " << buffer.front().tag << endl;
+			cout << "Writing frame " << buffer.front().tag << " " << buffer.front().image << endl;
 			buffer.erase(buffer.begin());
 			next++;
+			
 		}
 		
 		std::cout << "buffer contains:";
@@ -53,11 +60,12 @@ int main (int argc, char * argv[]) {
 	threadSerializer.detach();
 	
 	taggedFrame elements[] = { { .tag = 2 },
-							 { .tag = 3 },
+							 { .tag = 1 },
+							 { .tag = 0 },
 							 { .tag = 5 },
 							 { .tag = 4 },
 							 { .tag = 8 },
-							 { .tag = 1 },
+							 { .tag = 3 },
 							 { .tag = 6 },
 							 { .tag = 7 },
 							 { .tag = 9 } };
@@ -65,17 +73,15 @@ int main (int argc, char * argv[]) {
 	int size = sizeof(elements)/sizeof(*elements);
 	
 	for (int i=0; i< size; i++) {
-		
-		std::this_thread::sleep_for(chrono::milliseconds(500));	
-		
+		std::this_thread::sleep_for(chrono::milliseconds(500));
+
 		std::unique_lock<mutex> locker(mu);
-		
+
 		cout << "appending element " << elements[i].tag << endl;
 		buffer.push_back(elements[i]);
-		
+
 		locker.unlock();
 		cond.notify_one();
-		
 	}
 	
 	while (!buffer.empty()) {
